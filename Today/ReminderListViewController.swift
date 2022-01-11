a26bcebf7f3b79da47b8d81be330e2288d42dd75
g4eng <gaeng1107@gmail.com>
2022-01-11 18:14:12 +0900

    220111

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
+class ReminderListViewController: UITableViewController {
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+           let destination = segue.destination as? ReminderDetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            let reminder = Reminder.testData[indexPath.row]
+            destination.configure(with: reminder)
+        }
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+extension ReminderListViewController {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    // Use the override keyword anytime a subclass implements a method already implemented by its superclass.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderListCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        cell.titleLabel.text = reminder.title
+        cell.dateLabel.text = reminder.dueDate.description
+        cell.doneButton.setBackgroundImage(image, for: .normal)
+        cell.doneButtonAction = {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        return cell
     }
 }
