36392b732faaa29e4ca44966601102d66654c00c
g4eng <gaeng1107@gmail.com>
2022-01-12 23:44:37 +0900

    220112

--- Today/ReminderDetailViewController.swift
+++ Today/ReminderDetailViewController.swift
@@ -8,58 +8,43 @@
 import UIKit
 
 class ReminderDetailViewController: UITableViewController {
-    enum ReminderRow: Int, CaseIterable {
-        case title
-        case date
-        case time
-        case notes
-        
-        func displayText(for reminder: Reminder?) -> String? {
-            switch self {
-            case .title:
-                return reminder?.title
-            case .date:
-                return reminder?.dueDate.description
-            case .time:
-                return reminder?.dueDate.description
-            case .notes:
-                return reminder?.notes
-            }
-        }
-        
-        var cellImage: UIImage? {
-            switch self {
-            case .title:
-                return nil
-            case .date:
-                return UIImage(systemName: "calendar.circle")
-            case .time:
-                return UIImage(systemName: "clock")
-            case .notes:
-                return UIImage(systemName: "square.and.pencil")
-            }
-        }
-    }
-    
-    var reminder: Reminder?
-    
+
+    private var reminder: Reminder?
+    private var dataSource: UITableViewDataSource?
+
     func configure(with reminder: Reminder) {
         self.reminder = reminder
     }
-}
 
-extension ReminderDetailViewController {
-    static let reminderDetailCellIdentifier = "ReminderDetailCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ReminderRow.allCases.count
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setEditing(false, animated: false)
+        navigationItem.setRightBarButton(editButtonItem, animated: false)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
+    }
+
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        guard let reminder = reminder else {
+            fatalError("No reminder found for detail view")
+        }
+        if editing {
+            dataSource = ReminderDetailEditDataSource(reminder: reminder) { reminder in
+                self.editButtonItem.isEnabled = true
+            }
+            navigationItem.title = NSLocalizedString("Edit Reminder", comment: "edit reminder nav title")
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTriger))
+        } else {
+            dataSource = ReminderDetailViewDataSource(reminder: reminder)
+            navigationItem.title = NSLocalizedString("View Reminder", comment: "view reminder nav title")
+            navigationItem.leftBarButtonItem = nil
+            editButtonItem.isEnabled = true
+        }
+        tableView.dataSource = dataSource
+        tableView.reloadData()
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderDetailCellIdentifier, for: indexPath)
-        let row = ReminderRow(rawValue: indexPath.row)
-        cell.textLabel?.text = row?.displayText(for: reminder)
-        cell.imageView?.image = row?.cellImage
-        return cell
+    @objc func cancelButtonTriger() {
+        setEditing(false, animated: true)
     }
 }
---
+++ Today/Views/ReminderListCell.swift
@@ -0,0 +1,32 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by gaeng on 2022/01/11.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    // Type aliases are helpful for referring to an existing type with a name that’s more expressive.
+    typealias DoneButtonAction = () -> Void
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+    // This property prevents objects outside this class from executing this code.
+    private var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggered(_ sender: UIButton) {
+        doneButtonAction?()
+    }
+    
+    func configure(title: String, dateText: String, isDone: Bool, doneButtonAction: @escaping DoneButtonAction) {
+        titleLabel.text = title
+        dateLabel.text = dateText
+        let image = isDone ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        doneButton.setBackgroundImage(image, for: .normal)
+        self.doneButtonAction = doneButtonAction
+    }
+}
